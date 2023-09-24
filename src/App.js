8d266889b4de30bcefc9afc9060fa5ebee8406e0
Timbekki <127877414+Timbekki@users.noreply.github.com>
2023-09-24 12:01:26 +0300

    Demo example done during lectures.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { FaShoppingCart, IconName } from "react-icons/fa";
 
 function App() {
+  
+  const [cart,setCart] = useState([])
+
+  const [products, setProducts] = useState([
+    {name: "Jackets", price: 100}, 
+    {name: "Pants", price: 50},
+    {name: "Hats", price: 10}
+  ])
+
+//Function Buy
+  const buy = (e, index) => {
+    e.preventDefault()
+    const product = products[index]
+    setCart([...cart, product])
+    
+
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div id="container">
+    <h1>My Web Shop</h1>
+    <p><FaShoppingCart /> {cart.length}</p>
+    {
+      products.map((product,index) => (
+        <div key={index}>
+          <form onSubmit={(e) => buy (e,index)}>
+            <h3>{product.name}</h3>
+            <p>{product.price}€</p>
+            <button>Buy</button>
+          </form>
+        </div>
+
+      ))
+    }
+  </div>
+ );
 }
 
 export default App;
